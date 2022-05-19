a98b0b1977c3065d63ed37a45243d858704453cb
sondreb <sondreb@users.noreply.github.com>
2022-05-19 12:29:13 +0000

    deploy: beeb37e495388906734acf48696c43f9b74c20a0

--- docker/CITY/WALLET/wallet.sh
+++ docker/CITY/WALLET/wallet.sh
@@ -1,15 +1,18 @@
-echo "Blockcore Wallet // Hosting instance type: \"$1\" and version \"$2\"";
+INSTANCE="smartcityplatform"
+VERSION=$1
+
+echo "Blockcore Wallet // Hosting instance type: \"$INSTANCE\" and version \"$VERSION\"";
 
 #apt-get update \
 #  && apt-get install -y curl
 #sudo apt-get install zip unzip
 
-curl -Ls https://github.com/block-core/blockcore-extension/releases/download/$2/$1-$2.zip --output ./$1-$2.zip
+curl -Ls https://github.com/block-core/blockcore-extension/releases/download/$VERSION/$INSTANCE-$VERSION.zip --output ./$INSTANCE-$VERSION.zip
 
 rm -rf www
 mkdir www
 
-unzip $1-$2.zip -d ./www
+unzip $INSTANCE-$VERSION.zip -d ./www
 
 echo "Unpack completed, starting docker container to serve..."
 
