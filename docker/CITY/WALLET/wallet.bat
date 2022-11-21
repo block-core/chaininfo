7161d63e981c2f67c061fb5f8af9514d393eea79
Dan Gershony <dan.gershony@gmail.com>
2022-11-21 22:16:21 +0000

    Add files via upload

---
+++ docker/CITY/WALLET/wallet.bat
@@ -0,0 +1,21 @@
+set INSTANCE=coinvault
+set VERSION=%1
+
+echo "Blockcore Wallet // Hosting instance type: \"%INSTANCE%\" and version \"%VERSION%\"";
+
+@REM #apt-get update \
+@REM #  && apt-get install -y curl
+@REM #sudo apt-get install zip unzip
+
+
+powershell Invoke-WebRequest -Uri https://github.com/block-core/blockcore-extension/releases/download/%VERSION%/%INSTANCE%-%VERSION%.zip -OutFile ./%INSTANCE%-%VERSION%.zip
+
+del www -Recurse -Force
+mkdir www
+
+powershell Expand-Archive %INSTANCE%-%VERSION%.zip -DestinationPath www
+
+echo "Unpack completed, starting docker container to serve..."
+
+call docker-compose -p "COINVAULTWALLET" up -d
+call docker restart coinvault-wallet
